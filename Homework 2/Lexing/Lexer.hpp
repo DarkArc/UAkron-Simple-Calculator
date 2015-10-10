d354fe4c7e6063a8d1385b9e02207c9c94cd8b20
Wyatt Childers <wchilders@nearce.com>
2015-10-10 14:21:41 -0400

    Being work on Lexing & tokenizing

---
+++ Homework 2/Lexing/Lexer.hpp
@@ -0,0 +1,17 @@
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+#include "Token.hpp"
+
+#include <list>
+#include <iosfwd>
+
+struct Lexer {
+
+};
+
+std::list<token> tokenize(std::istream input) {
+
+}
+
+#endif
---
+++ Homework 2/Lexing/Token.hpp
@@ -0,0 +1,32 @@
+#ifndef TOKEN_HPP
+#define TOKEN_HPP
+
+enum class TokType {
+  L_PAREN,
+  R_PAREN,
+  PLUS,
+  MINUS,
+  STAR,
+  SLASH,
+  PERCENT,
+  KW_TRUE,
+  KW_FALSE,
+  AND,
+  OR,
+  NOT,
+  EQUAL,
+  NOT_EQ,
+  LESS_THAN,
+  LESS_THAN_EQ,
+  GREATER_THAN,
+  GREATHER_THAN_EQ,
+  INT
+};
+
+struct Token {
+  TokType type;
+  std::string text;
+
+};
+
+#endif
